45aa6d86a984b029b95b629be1bd321698715e18
MatsubaTakeru <matsuba.wkym.5611@gmail.com>
2026-05-30 15:42:09 +0900

    v0.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,19 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //
+        //player人数設定
+        //画面配置
+        //
+
+        //ゲーム開始
+            //トランプ配る(+デススタンプ)
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // OneGameManagerを開始
+        // 終わり判定
     }
 }
