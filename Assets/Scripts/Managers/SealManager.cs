921b8934ca58c7add69709e8fe6510a6d8e1623b
MatsubaTakeru <matsuba.wkym.5611@gmail.com>
2026-05-31 14:20:42 +0900

    phase 5

---
+++ Assets/Scripts/Managers/SealManager.cs
@@ -0,0 +1,21 @@
+namespace PokerGame
+{
+    public class SealManager
+    {
+        // ŠO•”ŒöŠJ
+        public ObserveResult ObserveCard(
+            Player player,
+            Card card)
+
+        // “à•”ˆ—
+        private void AddSealCount(
+            Player player,
+            DeathSeal seal)
+
+        private bool CheckDeath(
+            Player player)
+
+        private void Kill(
+            Player player)
+    }
+}
